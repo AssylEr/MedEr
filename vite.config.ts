e73665e34ae2e86e859dec9ec2bee32e68b0b559
AssylEr <assylerr2021@gmail.com>
2025-10-22 13:05:44 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import { fileURLToPath } from 'url';
 
+
+// Fix: Define `__dirname` for ES module scope.
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
